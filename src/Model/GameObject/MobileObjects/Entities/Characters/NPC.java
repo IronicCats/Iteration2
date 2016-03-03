0e6760a01d1590f68091c8ed8d9331b64eeef7cf
Michael <mikewim@ufl.edu>
2016-03-03 04:01:09 -0500

    Initialized some MobileObjects classes. Set packages

---
+++ src/Model/GameObject/MobileObjects/Entities/Characters/NPC.java
@@ -0,0 +1,14 @@
+package Model.GameObject.MobileObjects.Entities.Characters;
+
+import Model.GameObject.MobileObjects.Entities.Entity;
+
+/**
+ * Created by Wimberley on 3/3/16.
+ */
+
+/* To be continued. Setting up packages
+
+ */
+public class NPC extends Entity {
+    // inventory = NPCinvetory
+}
---
+++ src/Model/GameObject/MobileObjects/Entities/Entity.java
@@ -0,0 +1,14 @@
+package Model.GameObject.MobileObjects.Entities;
+
+import Model.GameObject.MobileObjects.MobileObject;
+
+/**
+ * Created by Wimberley on 3/3/16.
+ */
+
+/* to be continued. Just setting up packages
+
+ */
+public abstract class Entity extends MobileObject {
+    // private Inventory inventory;
+}
