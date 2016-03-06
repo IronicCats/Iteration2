568f8727478003829f9be898e623e9981a811163
Andres Borjas <aborjas@ufl.edu>
2016-03-06 18:16:04 -0500

    jesus my comments are so good. bow down to well commented code

--- src/Utilities/SaveLoad.java
+++ src/Utilities/SaveLoad.java
@@ -26,43 +26,58 @@ import java.io.File;
 public class SaveLoad {
 
 
-    private static String fileName;
-    public static SaveLoad instance = new SaveLoad();
+    private static String currFileName;     //the name of the file that will be saved.
+    public static SaveLoad instance = new SaveLoad(); //an instance of SaveLoad, only needs one.
     private static Entity player;   //will probably need an Entity list
     private static Map gameMap;     //list of all maps may be needed
+    //private static final String filePathExtension = Utilities.getFileSystemDependentPath("src/res/saveFiles";)
 
     public static Entity getPlayer(){// this will be changed later
         return player;
     }
-    public static Map getGameMap(){
+    public static Map getGameMap(){// to save the map
         return gameMap;
     }
 
-    public static void setGameMap(Map map){
+    public static void setGameMap(Map map){// sets the current map
         gameMap = map;
     }
-    public static void setPlayer(Entity a){
+    public static void setPlayer(Entity a){//sets the current player
         player = a;
     }
 
-    public static SaveLoad getInstance() {
+    public static SaveLoad getInstance() {//returns the instance of SaveLoad
         return instance;
     }
+    //---------------------------------LOAD--------------------------------// ヽ༼ຈل͜ຈ༽ﾉ
+    public static void load(String fileName){
+        currFileName = fileName;
+        //String filePath = filePathExtension + fileName;
+        String filePath = "src/res/saveFiles" + fileName;
+    }
 
-    public static void save(){
+
+
+    //---------------------------------SAVE--------------------------------// ヽ༼ຈل͜ຈ༽ﾉ
+    public static void save(){//function that will be called when you want to save
 
         try{
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-            Document doc = docBuilder.newDocument();
+            //Defines a factory API that enables applications to obtain a parser that produces DOM object trees from XML documents.
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance(); //create an instance of a documentBuilderFactory
+            //Defines the API to obtain DOM Document instances from an XML document. Using this class, an application programmer can obtain a Document from XML.
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder(); //creates a documentBuilder from Factory
+            Document doc = docBuilder.newDocument();                        //and creates a doc from Builder
 
-            String filePath = "/res/saveFiles" + fileName;
+            String filePath = "/res/saveFiles" + currFileName;              //the filePath is in resource folders plus passed in filename
 
-            Element rootElement = doc.createElementNS(filePath,"SaveFile");
-            doc.appendChild(rootElement);
+            //The Element interface represents an element in an HTML or XML document
+            Element rootElement = doc.createElementNS(filePath,"SaveFile"); //starts the root Element of XML from filePath
+            doc.appendChild(rootElement);                                   //adds the child to the doc
 
-            rootElement.appendChild(getEntity(doc,player));
+            rootElement.appendChild(getEntity(doc,player));                 //adds the next root element which is entities
            // rootElement.appendChild(getMap(doc,gameMap));
+
+            toXML(doc,filePath);                                            //turns the document into an XML
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -71,45 +86,49 @@ public class SaveLoad {
     }
 
     private static Node getEntity(Document doc, Entity e){
-        Element entity = doc.createElement("entities");
+        Element entity = doc.createElement("entities"); // gets entity with createElement
 
-        //entity.appendChild(getEntityInfo(doc,e));//need to make Entity info
+        entity.appendChild(getEntityInfo(doc,e));//need to make Entity info
         return entity;
     }
 
     private static Node getEntityInfo(Document doc, Entity e){
-        Element type = doc.createElement("player");
+        Element type = doc.createElement("player"); //creates a new element in document, labels it player
 
        // Attr etype = doc.createAttribute("type");
        // etype.setValue(e.getType());
 
-        Attr x = doc.createAttribute("locX");
-        x.setValue(Integer.toString(e.getLocation().getX()));
-        type.setAttributeNode(x);
+        Attr x = doc.createAttribute("locX"); //creates an attribute for X location
+        x.setValue(Integer.toString(e.getLocation().getX())); //sets the x attriubte to player x
+        type.setAttributeNode(x);   //sets the attribute of element as x.
 
-        Attr y = doc.createAttribute("locY");
-        y.setValue(Integer.toString(e.getLocation().getY()));
-        type.setAttributeNode(y);
+        Attr y = doc.createAttribute("locY"); //creates an attribute for Y location
+        y.setValue(Integer.toString(e.getLocation().getY())); //sets the y attribute to player y
+        type.setAttributeNode(y);  //element sets attribute Node to y
 
-        Attr direction = doc.createAttribute("direction");
-        direction.setValue(Integer.toString(e.getLocation().getDir()));
-        type.setAttributeNode(direction);
+        Attr direction = doc.createAttribute("direction"); //creates an attribute for direction
+        direction.setValue(Integer.toString(e.getLocation().getDir()));//sets the direction attribute to player direction
+        type.setAttributeNode(direction); //element sets attribute Node to direction
 
-        Attr occupation = doc.createAttribute("occupation");
-        occupation.setValue(e.getOccupation().getName());
-        type.setAttributeNode(occupation);
+        Attr occupation = doc.createAttribute("occupation"); //creates an attribute for occupation
+        occupation.setValue(e.getOccupation().getName());   //sets the occupation attribute to player occupation
+        type.setAttributeNode(occupation);          //element sets attribute Node to element
 
-        return type;
+        return type;                //returns it so it can be used in XML
     }
 
     public static void toXML(Document doc, String fileName){
-        TransformerFactory tf = TransformerFactory.newInstance();
+        TransformerFactory tf = TransformerFactory.newInstance(); //creates an instance of a Transformer factory for xml shenanigans
         try{
-            Transformer transformer = tf.newTransformer();
+            Transformer transformer = tf.newTransformer();      //creates a new tranformer from the factory
+
+            //Acts as a holder for a transformation Source tree in the form of a Document Object Model (DOM) tree.
             DOMSource dSource = new DOMSource(doc);
 
-            StreamResult sResult = new StreamResult(new File(fileName));
+            //cts as an holder for a transformation result, which may be XML
+            StreamResult sResult = new StreamResult(new File(fileName));//streamResult for file.
 
+            //Set an output property that will be in effect for the transformation
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             tf.setAttribute("indent-number",5);
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount",
