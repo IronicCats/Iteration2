7f61345fb362a1fe9a3a5fbeb0415941db6c8753
Kyle Brost <kjbrost@gmail.com>
2016-03-06 14:30:33 -0500

    initial commit.  factory pattern to create NPCs.

--- src/Utilities/NPCFactory.java
+++ src/Utilities/NPCFactory.java
@@ -1,7 +1,36 @@
 package Utilities;
 
+import Model.GameObject.MobileObjects.Entities.Entity;
+
 /**
  * Created by broskj on 3/6/16.
  */
 public class NPCFactory {
-}
+    public static void /*Entity*/ makeNPC(NPCEnum npcEnum) {
+        switch (npcEnum) {
+            case KITTEN:
+            case SMALL_CAT:
+            case FAT_CAT:
+            case CORGI_SHOPKEEPER:
+            case WOLF_SHOPKEEPER:
+            case DAVE_PET:
+            case SHEEP_VEHICLE:
+            default:
+                break; /* return null; */
+        }
+    } // end makeNPC
+
+    public static void /*NPCView*/ makeAsset(NPCEnum npcEnum, Entity entity) {
+        switch (npcEnum) {
+            case KITTEN:
+            case SMALL_CAT:
+            case FAT_CAT:
+            case CORGI_SHOPKEEPER:
+            case WOLF_SHOPKEEPER:
+            case DAVE_PET:
+            case SHEEP_VEHICLE:
+            default:
+                break; /* return null; */
+        }
+    } // end makeAsset
+} // end class NPCFactory
