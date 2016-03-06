187dc4adaeee6a31ddb6380f2b3c8f6c000ad2e3
dnkllhr <dnkllhr@gmail.com>
2016-03-06 18:33:30 -0500

    fixed

--- src/Utilities/AIUtilities/Astar.java
+++ src/Utilities/AIUtilities/Astar.java
@@ -1,16 +1,37 @@
 package Utilities.AIUtilities;
 
 import Model.Location;
+import Utilities.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Aidan on 3/6/2016.
  */
+
 public class Astar {
 
-    public Location[] FindPath(int startX, int startY, int endX, int endY){
+    private Map map;
+    private int maxDistance = Settings.GAMEHEIGHT * Settings.GAMEWIDTH;
+    private Node[][] nodes;
+    List<Node> openList = new ArrayList<Node>();
+    List<Node> closedList = new ArrayList<Node>();
+
+    public List FindPath(Location start, Location end){
+
+        //if end point is blocked return null because there is nothing there
+
+        
+        openList.add(new Node(start));
+
+
+        boolean done = false;
 
 
 
+        return openList;
     }
 
 }
