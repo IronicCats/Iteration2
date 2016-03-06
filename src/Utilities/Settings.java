1020981bd381360bd0d3eb13409259ecddb4fe48
Kyle Brost <kjbrost@gmail.com>
2016-03-06 13:54:57 -0500

    added keycodes to Settings for use in GameController

--- src/Utilities/Settings.java
+++ src/Utilities/Settings.java
@@ -13,12 +13,12 @@ public class Settings {
     public static int TILEWIDTH = 64;
     public static int TILEHEIGHT = 56;
 
-    public static int UP_RIGHT = 45;
-    public static int UP = 90;
-    public static int UP_LEFT = 135;
-    public static int DOWN_LEFT = 225;
-    public static int DOWN = 270;
-    public static int DOWN_RIGHT = 315;
+    public static int UP_RIGHT = 105;
+    public static int UP = 104;
+    public static int UP_LEFT = 103;
+    public static int DOWN_LEFT = 97;
+    public static int DOWN = 98;
+    public static int DOWN_RIGHT = 99;
 
     public static String TITLE = "Ironic Cats";
 }
