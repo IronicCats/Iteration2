01f6964679f3797a75bda78abe5974ae59815e21
Kyle Brost <kjbrost@gmail.com>
2016-03-08 17:52:03 -0500

    made basic class structure

--- src/Model/GameObject/MobileObjects/Entities/Pet.java
+++ src/Model/GameObject/MobileObjects/Entities/Pet.java
@@ -1,8 +1,10 @@
 package Model.GameObject.MobileObjects.Entities;
 
+import Model.Inventory.Pack;
 import Model.Stats.PetStats;
 import Model.GameObject.MobileObjects.MobileObject;
 import Model.Location;
+import Utilities.Subject;
 
 /**
  * Created by Wimberley on 3/3/16.
@@ -12,10 +14,34 @@ import Model.Location;
 
  */
 public class Pet extends MobileObject {
-    // inventory = pet Inventory();
     PetStats stats;
+    Pack pack;
+    boolean owned;
+
+    public Pet() {
+        super(new Location(0, 0));
+        stats = new PetStats();
+        pack = new Pack();
+        owned = false;
+    } // end default constructor
 
     public Pet(Location location) {
         super(location);
-    }
-}
+        stats = new PetStats();
+        pack = new Pack();
+        owned = false;
+    } // end constructor
+
+    public Pet(Location location, PetStats stats, Pack pack, boolean owned) {
+        super(location);
+        this.stats = stats;
+        this.pack = pack;
+        this.owned = owned;
+    } // end constructor
+
+    
+
+    public PetStats getStats() { return stats; }
+    public Pack getPack() { return pack; }
+    public boolean getOwned() { return owned; }
+} // end
