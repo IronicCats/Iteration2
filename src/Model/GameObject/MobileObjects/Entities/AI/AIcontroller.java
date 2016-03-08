bc4ed0c6f154ccc7d5a3de108c3e45d9d8368a12
dnkllhr <dnkllhr@gmail.com>
2016-03-07 19:24:55 -0500

    working

--- src/Model/GameObject/MobileObjects/Entities/AI/AIcontroller.java
+++ src/Model/GameObject/MobileObjects/Entities/AI/AIcontroller.java
@@ -1,8 +1,33 @@
 package Model.GameObject.MobileObjects.Entities.AI;
 
+import Model.Map.Tile;
+import Model.Tickable;
+import Utilities.AIUtilities.Astar;
+
+import java.util.ArrayList;
+
 /**
  * Created by Aidan on 3/6/2016.
  */
-public abstract class AIcontroller {
+public abstract class AIcontroller implements Tickable {
 
+    private Astar astar;
+
+    public AIcontroller(Astar astar){
+        this.astar = astar;
+    }
+
+    public void move(Tile startTile, Tile endTile){
+
+        ArrayList<Tile> path = new ArrayList<Tile>();
+        path = astar.Findpath(startTile,endTile);
+
+
+    }
+
+
+    @Override
+    public void tick() {
+            
+    }
 }
