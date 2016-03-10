db2877f51b604db37a01216f328b7e94d9eb7ae1
m-mazumder <m-mazumder>
2016-03-09 19:25:37 -0500

    Added applyEffect

--- src/Model/GameObject/MobileObjects/Entities/Characters/Character.java
+++ src/Model/GameObject/MobileObjects/Entities/Characters/Character.java
@@ -1,5 +1,6 @@
 package Model.GameObject.MobileObjects.Entities.Characters;
 
+import Model.Effects.Effect;
 import Model.GameObject.Item.Item;
 import Model.GameObject.Item.Items.Takable;
 import Model.GameObject.MobileObjects.Entities.Characters.Occupation.Occupation;
@@ -62,5 +63,12 @@ public abstract class Character extends Entity {
         DisplayMessage.addMessage(new GameMessage("You emptied your Pack", 3));
         getTile().addItems(inventory.emptyPack());
     } // end emptyPack
+
+    //effect being applied to an entity
+    public void applyEffect(Effect e){
+        ((CharacterStats)getStats()).applyEffect(e);
+    }// end applyEffect
+
+    
 
 } // end class Character
