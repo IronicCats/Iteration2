ad9a67d9637943703a7b9bf57b49ff25c668f3a8
Spencer Reyka <nellafeht06@ufl.edu>
2016-03-09 20:54:31 -0500

    states are connected with views and controllers

--- src/View/Views/SaveView.java
+++ src/View/Views/SaveView.java
@@ -1,7 +1,19 @@
 package View.Views;
 
+import View.View;
+import View.ViewUtilities.Renderable;
+
+import java.awt.*;
+
 /**
  * Created by Dartyx on 3/7/2016.
  */
-public class SaveView {
+public class SaveView implements Renderable{
+    private int width,height;
+    public SaveView(){
+
+    }
+    public void render(Graphics g){
+
+    }
 }
