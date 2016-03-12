2814ebc88fd533cbdc2bdeeb6058628ec9a1fcf5
dnkllhr <dnkllhr@gmail.com>
2016-03-12 17:25:39 -0500

    got rid of print statements

--- src/Model/GameObject/MobileObjects/Entities/AI/EnemyController.java
+++ src/Model/GameObject/MobileObjects/Entities/AI/EnemyController.java
@@ -4,6 +4,8 @@ import Model.GameObject.MobileObjects.Entities.AI.AIController;
 import Model.GameObject.MobileObjects.Entities.Characters.HostileNPC;
 import Model.Map.Map;
 import Utilities.AIUtilities.DirectionofTarget;
+import Utilities.AIUtilities.DistanceFromFaceableTarget;
+import Utilities.AbilitiesUtilities.checkAbilityRange;
 
 /**
  * Created by Aidan on 3/7/2016.
@@ -33,12 +35,11 @@ public class EnemyController extends AIController {
 
     public void followThenAttackinRange() {
         if (canFace()) {
-            System.out.println(DirectionofTarget.getDir(AI.getLocation(), target.getLocation()));
-            if (DirectionofTarget.getDir(AI.getLocation(), target.getLocation()) != 0) {
-                AI.face(DirectionofTarget.getDir(AI.getLocation(), target.getLocation()));
-                //enemy.attack(checkAbilityRange.check(enemy.getOccupation().getOccupationalAbilities(),DistanceFromFaceableTarget.calculate(enemy,target)).get(0));
+            if (DirectionofTarget.getDir(enemy.getLocation(), target.getLocation()) != 0) {
+                enemy.face(DirectionofTarget.getDir(enemy.getLocation(), target.getLocation()));
+//                enemy.attack(checkAbilityRange.check(enemy.getOccupation().getOccupationalAbilities(), DistanceFromFaceableTarget.calculate(enemy,target)).get(0));
             }
-            //follow();
+            follow();
         }
     }
 
