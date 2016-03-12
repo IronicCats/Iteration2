ac9e774db8cd1190a9a8fc560e1513cee221cad1
Joshua Kegley <josh1289op@gmail.com>
2016-03-12 15:58:29 -0500

    Mike and Aidan writes shitty code

--- src/View/ViewUtilities/ViewModules/ViewModule.java
+++ src/View/ViewUtilities/ViewModules/ViewModule.java
@@ -24,15 +24,15 @@ public class ViewModule {
         double green = 0.0;
         double red = 0.0;
         if( 0<=power && power <0.5 ) { //first, green stays at 100 %, red raises to 100 %
-                green = 1.0;
-        }
-        red = 2 * power;
-        if( 0.5<=power && power <=1 ){ //then red stays at 100 %, green decays
                 red = 1.0;
         }
-        green = 1.0 - 2 * (power-0.5);
+        green = 2 * power;
+        if( 0.5<=power && power <=1 ){ //then red stays at 100 %, green decays
+                green = 1.0;
+        }
+        red = 1.0 - 2 * (power-0.5);
 
-        return new Color((int)red*255, (int)green*255, (int)blue*255);
+        return new Color((int) red*255, (int)green*255, (int)blue*255);
         //return Color.getHSBColor((float)(1 - ratio), 1, 1);
     }
 
