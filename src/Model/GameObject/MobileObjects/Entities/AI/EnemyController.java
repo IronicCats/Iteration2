4d918d590d1a829770dbc945ceb119a1f8a74d31
dnkllhr <dnkllhr@gmail.com>
2016-03-12 18:02:49 -0500

    fninshed when following Attack inRange

--- src/Model/GameObject/MobileObjects/Entities/AI/EnemyController.java
+++ src/Model/GameObject/MobileObjects/Entities/AI/EnemyController.java
@@ -1,5 +1,6 @@
 package Model.GameObject.MobileObjects.Entities.AI;
 
+import Model.Abilities.Abilities;
 import Model.GameObject.MobileObjects.Entities.AI.AIController;
 import Model.GameObject.MobileObjects.Entities.Characters.HostileNPC;
 import Model.Map.Map;
@@ -13,6 +14,7 @@ import Utilities.AbilitiesUtilities.checkAbilityRange;
 public class EnemyController extends AIController {
 
     HostileNPC enemy;
+
 
     public EnemyController(Map map) {
         super(map);
@@ -35,12 +37,17 @@ public class EnemyController extends AIController {
 
     public void followThenAttackinRange() {
         if (canFace()) {
-            if (DirectionofTarget.getDir(enemy.getLocation(), target.getLocation()) != 0) {
-                enemy.face(DirectionofTarget.getDir(enemy.getLocation(), target.getLocation()));
-//                enemy.attack(checkAbilityRange.check(enemy.getOccupation().getOccupationalAbilities(), DistanceFromFaceableTarget.calculate(enemy,target)).get(0));
+            enemy.face(DirectionofTarget.getDir(enemy.getLocation(), target.getLocation()));
+            Abilities a = checkAbilityRange.check(enemy.getOccupation().getOccupationalAbilities());
+            if (a.getRange() >= DistanceFromFaceableTarget.calculate(enemy, target)) {
+                //enemy.attack(a);
+            } else {
+                follow();
             }
-            follow();
         }
+            else {
+                follow();
+            }
     }
 
     @Override
