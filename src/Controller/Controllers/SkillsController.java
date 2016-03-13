7a958f46ce11d6e30b695f394fd639dc96757f1e
Kyle Brost <kjbrost@gmail.com>
2016-03-13 15:51:24 -0400

    initial commit; used for displaying and modifying available skills

---
+++ src/Controller/Controllers/SkillsController.java
@@ -0,0 +1,7 @@
+package Controller.Controllers;
+
+/**
+ * Created by broskj on 3/13/16.
+ */
+public class SkillsController {
+}
---
+++ src/State/States/GameState/SkillsState.java
@@ -0,0 +1,7 @@
+package State.States.GameState;
+
+/**
+ * Created by broskj on 3/13/16.
+ */
+public class SkillsState {
+}
