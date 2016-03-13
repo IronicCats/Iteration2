1eda64d0021b35c22e168420c32a48738e06758e
m-mazumder <m-mazumder>
2016-03-12 23:13:22 -0500

    Whatever

--- src/View/Views/MobileObjectView.java
+++ src/View/Views/MobileObjectView.java
@@ -112,7 +112,6 @@ public class MobileObjectView implements Renderable, Observer {
 
     public void render(Graphics g, int cameraXOffset, int cameraYOffset) {
         if (entity instanceof Character && ((Character) entity).isDead()) {
-            System.out.println("Dead");
             return;
         }
         tween();
