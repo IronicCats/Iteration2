d78eeb59f7d316a90f9b471d688832cac1ec156c
m-mazumder <m-mazumder>
2016-03-14 04:47:29 -0400

    Observe

--- src/Model/Abilities/Observe.java
+++ src/Model/Abilities/Observe.java
@@ -1,9 +1,15 @@
 package Model.Abilities;
 
+import Model.GameObject.AreaEffect.AreaEffectEnum;
+import Model.GameObject.MobileObjects.Entities.Characters.Occupation.Occupation;
+import Model.GameObject.MobileObjects.Entities.Characters.Player;
 import Model.GameObject.MobileObjects.MobileObject;
 import Model.Map.Tile;
+import View.Views.MessageBox.DisplayMessage;
+import View.Views.MessageBox.GameMessage;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by TARIT on 3/14/2016.
@@ -16,5 +22,27 @@ public class Observe extends PassiveAbility {
 
     public void checkSurroundingTiles(ArrayList<Tile> tiles) {
 
+        Iterator<Tile> i = tiles.iterator();
+        while(i.hasNext()){
+            Tile ti = i.next();
+            int offensiveRating = (((Player)(ti.getObject())).getStats().getOffensiveRating());
+            Occupation occupation = (((Player)(ti.getObject())).getOccupation());
+            int defensiveRating = (((Player)(ti.getObject())).getStats().getDefensiveRating());
+            if(skillLevel == 0){
+                if (ti.hasObject()){
+                   DisplayMessage.addMessage(new GameMessage(" Defensive Rating " + (defensiveRating - 3), 10));
+                }
+            }
+            else if(skillLevel == 1 ){
+                if (ti.hasObject()){
+                    DisplayMessage.addMessage(new GameMessage(" Defensive Rating " + defensiveRating + "\n" + " Occupation" + occupation, 10));
+                }
+            }
+            else{
+                if (ti.hasObject()){
+                    DisplayMessage.addMessage(new GameMessage(" Offensive Rating " + offensiveRating , 10));
+                }
+            }
+        }
     }
 }
