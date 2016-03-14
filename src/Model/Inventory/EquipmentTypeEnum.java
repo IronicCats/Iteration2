26302049b91f29b3a51986cfeec0401a7039b2de
Kyle Brost <kjbrost@gmail.com>
2016-03-13 23:18:44 -0400

    created new equipment type for brawling

--- src/Model/Inventory/EquipmentTypeEnum.java
+++ src/Model/Inventory/EquipmentTypeEnum.java
@@ -4,5 +4,5 @@ package Model.Inventory;
  * Created by broskj on 3/6/16.
  */
 public enum EquipmentTypeEnum {
-    ONE_HANDED, TWO_HANDED, HEAD, CHEST, LEGS, GLOVES, BOOTS, SHIELD, ACCESSORY
+    ONE_HANDED, TWO_HANDED, DOUBLE_HANDED, HEAD, CHEST, LEGS, GLOVES, BOOTS, SHIELD, ACCESSORY
 }
